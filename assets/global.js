69ac85f71fc5261b8765597ddbc06f039f739e8e
simarjitsinghmann <simarjitsinghmann@Simarjits-MacBook-Pro.local>
2024-03-17 10:18:29 +0530

    Task b is done

--- assets/global.js
+++ assets/global.js
@@ -1,3 +1,4 @@
+alert('aasd')
 function getFocusableElements(container) {
   return Array.from(
     container.querySelectorAll(
@@ -1136,6 +1137,10 @@ class VariantSelects extends HTMLElement {
           `Sku-${this.dataset.originalSection ? this.dataset.originalSection : this.dataset.section}`
         );
         const skuDestination = document.getElementById(`Sku-${this.dataset.section}`);
+        const dimensionsDestination = document.getElementById(`variant-dimensions-${this.dataset.section}`);
+        const dimensionsSource = html.getElementById(
+          `variant-dimensions-${this.dataset.originalSection ? this.dataset.originalSection : this.dataset.section}`
+        );
         const inventorySource = html.getElementById(
           `Inventory-${this.dataset.originalSection ? this.dataset.originalSection : this.dataset.section}`
         );
@@ -1158,6 +1163,7 @@ class VariantSelects extends HTMLElement {
 
         if (source && destination) destination.innerHTML = source.innerHTML;
         if (inventorySource && inventoryDestination) inventoryDestination.innerHTML = inventorySource.innerHTML;
+        if (dimensionsSource && dimensionsDestination) dimensionsDestination.innerHTML = dimensionsSource.innerHTML;
         if (skuSource && skuDestination) {
           skuDestination.innerHTML = skuSource.innerHTML;
           skuDestination.classList.toggle('hidden', skuSource.classList.contains('hidden'));
